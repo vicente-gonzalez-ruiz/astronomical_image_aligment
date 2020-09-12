e05a75e03bbaa78757ff601bb74c5d17e3c11497
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2020-09-12 09:25:09 +0200

    .

--- match3.py
+++ match3.py
@@ -3,53 +3,33 @@ import cv2 as cv
 import os
 import astroalign as aa
 
+def read_image(fn):
+    print("Reading", fn, end= ' ', flush=True)
+    img = cv.imread("./input/" + fn, cv.IMREAD_UNCHANGED)
+    if img is None:
+        print("Error reading", fn)
+    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    print("with shape", img.shape)
+    return img.astype(np.float32)
+
 for root, dirs, files in os.walk("./input/"):
     files.sort()
+    first = files.pop(0)
+    target = read_image(first)
+    accumulated = target
+    counter = 1
     for filename in files:
-        fn = "./input/" + filename
-        print("Reading", fn)
-        img = cv.imread(fn, cv.IMREAD_UNCHANGED)
-        if img is None:
-            print("Error reading", fn)
-        img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        
+        source = read_image(filename)
+        print("Projecting", filename)
+        projection, footprint = aa.register(source, target,
+                                            detection_sigma = 2,
+                                            min_area = 9)
+        cv.imwrite("{:03d}.tif".format(counter), projection)
+        accumulated += projection
+        target = source
+        counter += 1
 
-images = []
-
-# Read the images
-for root, dirs, files in os.walk("./input/"):
-    files.sort()
-    for filename in files:
-        fn = "./input/" + filename
-        print("Reading", fn)
-        img = cv.imread(fn, cv.IMREAD_UNCHANGED)
-        if img is None:
-            print("Error reading", fn)
-        img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        images.append(img.astype(np.float32))
-
-first_image = images[0]
-rest_images = []
-for i in range(len(images)-1):
-    rest_images.append(images[i+1])
-
-projections = []
-counter = 0
-for image in rest_images:
-    print("Processing ...", end=' ', flush=True)
-    source = image #cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    target = first_image #cv.cvtColor(first_image, cv.COLOR_BGR2GRAY)
-    projection, footprint = aa.register(source, target)
-    cv.imwrite("{:03d}.tif".format(counter), projection)
-    counter += 1
-    projections.append(projection)
-    
-print("Projected", len(projections), "images")
-    
-accumulated = first_image
-for projection in projections:
-    accumulated += projection
-accumulated /= len(images)
+accumulated /= counter
 accumulated = accumulated.astype(np.uint8)
 
 cv.imwrite("output.tif", accumulated)
