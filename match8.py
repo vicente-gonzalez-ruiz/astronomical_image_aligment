62957dc7de1c9fa862f1e7083abc60ada76caf96
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2020-12-26 11:03:06 +0100

    .

--- match8.py
+++ match8.py
@@ -5,13 +5,13 @@ import astroalign as aa
 import image_io
 
 MAX_NUMBER_OF_IMAGES = 500
-MAX_CONTROL_POINTS = 50
+MAX_CONTROL_POINTS = 500
 INPUT_DIR = "/Users/vruiz/Pictures/jupiter-saturno 2020-12-24-c/"
 EXTENSION = ".tiff"
 
 # Dark image. This image registerizing tool supposes that all the dark
 # image is the same for all the input images.
-dark_image = image_io.read(INPUT_DIR + "dark" + EXTENSION)
+dark_image = image_io.read(INPUT_DIR + "dark" + EXTENSION).astype(np.float32)
 
 def normalize(image):
     max = image.max()
@@ -26,12 +26,12 @@ prefix = INPUT_DIR + "full_size/"
 for root, dirs, files in os.walk(prefix):
     files.sort()
     source_name = files.pop(0)
-    source_image = image_io.read(prefix + source_name)
+    source_image = image_io.read(prefix + source_name).astype(np.float32)
     source_image -= dark_image
     counter = 1
     for target_name in files:
         source_image_luma = cv.cvtColor(source_image, cv.COLOR_BGR2GRAY)
-        target_image = image_io.read(prefix + target_name)
+        target_image = image_io.read(prefix + target_name).astype(np.float32)
         target_image -= dark_image
         accumulated_image = target_image
         print("Projecting", source_name, "to", target_name)
